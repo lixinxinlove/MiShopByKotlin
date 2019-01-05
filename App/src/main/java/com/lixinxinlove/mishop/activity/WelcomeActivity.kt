9c7f9d9d3f75aebbea183638ca8ab228f70e46b6
lixinxinlove <895330766@qq.com>
2019-01-05 19:57:31 +0800

    欢迎页全屏显示

--- App/src/main/java/com/lixinxinlove/mishop/activity/WelcomeActivity.kt
+++ App/src/main/java/com/lixinxinlove/mishop/activity/WelcomeActivity.kt
@@ -3,11 +3,12 @@ package com.lixinxinlove.mishop.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.lixinxinlove.base.activity.BaseActivity
 import com.lixinxinlove.mishop.R
 import com.lixinxinlove.user.activity.LoginActivity
 import kotlinx.android.synthetic.main.activity_welcome.*
-import android.R.raw
 
 
 /**
@@ -22,10 +23,12 @@ class WelcomeActivity : BaseActivity() {
     override fun listener() {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         super.onCreate(savedInstanceState)
-
         rootView.postDelayed({
-           // startActivity(Intent(this, HomeActivity::class.java))
+            // startActivity(Intent(this, HomeActivity::class.java))
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }, 2000)
