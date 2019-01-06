e896f2c56eba9344dc7d5d4d1171965d86c34a12
lixinxinlove <895330766@qq.com>
2019-01-06 15:20:26 +0800

    添加网络请求库

---
+++ UserConter/src/main/java/com/lixinxinlove/user/data/respository/UserRepository.kt
@@ -0,0 +1,47 @@
+package com.lixinxinlove.user.data.respository
+
+import com.kotlin.base.data.net.RetrofitFactory
+import com.kotlin.base.data.protocol.BaseResp
+import com.kotlin.user.data.protocol.*
+import com.lixinxinlove.user.data.api.UserApi
+import rx.Observable
+
+class UserRepository{
+
+    /*
+       用户注册
+    */
+    fun register(mobile:String,pwd:String,verifyCode:String): Observable<BaseResp<String>> {
+        return RetrofitFactory.instance.create(UserApi::class.java).register(RegisterReq(mobile,pwd,verifyCode))
+    }
+
+    /*
+        用户登录
+     */
+    fun login(mobile:String,pwd:String,pushId:String): Observable<BaseResp<UserInfo>>{
+        return RetrofitFactory.instance.create(UserApi::class.java).login(LoginReq(mobile,pwd,pushId))
+    }
+
+    /*
+        忘记密码
+     */
+    fun forgetPwd(mobile:String,verifyCode:String): Observable<BaseResp<String>>{
+        return RetrofitFactory.instance.create(UserApi::class.java).forgetPwd(ForgetPwdReq(mobile,verifyCode))
+    }
+
+    /*
+        重置密码
+     */
+    fun resetPwd(mobile:String,pwd:String): Observable<BaseResp<String>>{
+        return RetrofitFactory.instance.create(UserApi::class.java).resetPwd(ResetPwdReq(mobile,pwd))
+    }
+
+    /*
+        编辑用户资料
+     */
+    fun editUser(userIcon:String,userName:String,userGender:String,userSign:String):Observable<BaseResp<UserInfo>>{
+        return RetrofitFactory.instance.create(UserApi::class.java).editUser(EditUserReq(userIcon,userName,userGender,userSign))
+    }
+
+
+}
