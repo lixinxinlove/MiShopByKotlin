c383514b686aaa777505c803272352109d96344b
lixinxin <895330766@qq.com>
2019-01-11 11:03:51 +0800

    offscreenPageLimit = 2

---
+++ PayConter/src/main/java/com/lixinxinlove/pay/activity/PayActivity.kt
@@ -0,0 +1,23 @@
+package com.lixinxinlove.pay.activity
+
+import android.os.Bundle
+import com.lixinxinlove.base.activity.BaseActivity
+import com.lixinxinlove.pay.R
+
+/**
+ * 支付
+ */
+class PayActivity : BaseActivity() {
+
+    override fun layoutId(): Int {
+        return R.layout.activity_pay
+    }
+
+    override fun listener() {
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
